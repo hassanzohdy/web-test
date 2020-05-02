6c40a58e72f853a723da48add8f903d3b2ff08c7
hasan <hassanzohdy@gmail.com>
2020-05-02 19:44:52 +0200

    Added user class

---
+++ User.php
@@ -0,0 +1,45 @@
+<?php 
+
+class User 
+{
+    /**
+     * Check if user is logged in either in session or cookies
+     * 
+     * @return bool
+     */
+    public function isLoggedIn(): bool 
+    {
+
+    }
+    
+    /**
+     * Get user name
+     * 
+     * @return string
+     */
+    public function name(): string 
+    {
+        
+    }
+    
+    /**
+     * Get user email
+     * 
+     * @return string
+     */
+    public function email(): string 
+    {
+        
+    }
+    
+    
+    /**
+     * Get user image path
+     * 
+     * @return string
+     */
+    public function image(): string 
+    {
+        
+    }
+}
